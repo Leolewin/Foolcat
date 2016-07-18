15cb207099455e80366e4a635f7c34c138e904f9
leolewin <leolewin@live.cn>
2016-07-18 11:56:21 +0800

    add some immature module which need further thinking

--- app/routes/api/wptapi.rb
+++ app/routes/api/wptapi.rb
@@ -1,12 +1,14 @@
 # Any Sample project Rest APIs locates here
-module Foolcat
-    module RestAPI
-        class WPTAPI < BaseAPI
-            
-            get '/rest/wptresult' do 
-	            return 'service is ready for use!'
-            end
-            
-        end
-    end
-end
+# module Foolcat
+#     module RestAPI
+#         class WPTAPI < BaseAPI
+#
+#             get '/rest/wptresult' do
+# 	            return 'service is ready for use!'
+#             end
+#
+#         end
+#     end
+# end
+
+#need further thinking on the module!!!!
